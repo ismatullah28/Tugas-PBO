0ae73f64c0134388c1a096c77148d765b8465c6e
ismatullah28 <73099819+ismatullah28@users.noreply.github.com>
2020-12-25 02:01:23 +0700

    Add files via upload

---
+++ soal3.java
@@ -0,0 +1,27 @@
+public class No_3 {
+
+    public static void main(String[] args) {
+
+        int a=5;
+
+        for (int i=1; i<=5; i++){
+
+            for (int j=a; j>=i; j--){
+
+                if (i < 3){
+
+                    System.out.print(i + " ");
+
+                }else
+
+                    System.out.print(8 - 5 + " ");
+
+            }
+
+            System.out.print("\n");
+
+        }
+
+    }
+
+}
---
+++ soal4.java
@@ -0,0 +1,10 @@
+public class Tugas4 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 5; x++) {
+            for (int y = x; y <= 5; y++) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+    }
+}
